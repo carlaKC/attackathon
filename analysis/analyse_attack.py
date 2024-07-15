bca2097a7798030c37d666054afd83db4ca1d7ce
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2024-07-15 10:52:26 -0400

    analysis: print results in csv format for easy export

--- analysis/analyse_attack.py
+++ analysis/analyse_attack.py
@@ -152,3 +152,6 @@ if __name__ == "__main__":
     print(f"- Attacker paid {attacker_to_target_percent}%: {attacker_to_target_total} msat")
     print(f"- Honest traffic paid {honest_to_target_percent}%: {honest_revenue} msat\n")
 
+    print("Result CSV:")
+    print("attacker_success_msat,attacker_unconditional_msat,target_success_msat,target_unconditional_msat,attacker_to_target_msat")
+    print(f"{attacker_success_msat},{attacker_unconditional_msat},{success_revenue},{unconditional_revenue},{attacker_to_target_total}")
