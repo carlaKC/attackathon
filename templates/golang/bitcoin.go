13fc69579bdda9e83ac77b1849273c966f07b98f
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2024-07-17 09:08:39 -0400

    nit: gofmt bitcoin.go

--- templates/golang/bitcoin.go
+++ templates/golang/bitcoin.go
@@ -8,12 +8,12 @@ import (
 
 // Mine n blocks using bitcoin cli.
 func Mine(n int) error {
-        // Santiy check for fat fingering to stop us setting our laptops on 
-        // fire, can be removed.
-        if n > 1000{
-                return fmt.Errorf("trying to mine: %v blocks failed "+
-                "santiy check", n)
-        }
+	// Santiy check for fat fingering to stop us setting our laptops on
+	// fire, can be removed.
+	if n > 1000 {
+		return fmt.Errorf("trying to mine: %v blocks failed "+
+			"santiy check", n)
+	}
 
 	cmd := exec.Command("bitcoin-cli", "-generate", strconv.Itoa(n))
 
