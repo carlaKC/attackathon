7860a27a2c41d9a4a94eb79e94c414c7b8a147ea
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2024-08-05 11:21:46 -0400

    temp: fast slot jamming ln_50

--- templates/copy_and_run_golang.sh
+++ templates/copy_and_run_golang.sh
@@ -9,8 +9,8 @@ kubectl cp golang warnet-armada/flagship:/
 if ! kubectl exec -it flagship -n warnet-armada -- bash -c 'cat run.sh | grep -q "go install"'; then
     echo "Adding instructions to run script"
 
-    kubectl exec -it flagship -n warnet-armada -- bash -c 'echo "export PEER=6" >> /run.sh'
-    kubectl exec -it flagship -n warnet-armada -- bash -c 'echo "export RESOURCES=LIQUIDITY" >> /run.sh'
+    kubectl exec -it flagship -n warnet-armada -- bash -c 'echo "export PEER=44" >> /run.sh'
+    kubectl exec -it flagship -n warnet-armada -- bash -c 'echo "export RESOURCES=SLOTS" >> /run.sh'
     kubectl exec -it flagship -n warnet-armada -- bash -c 'echo "export SPEED=FAST" >> /run.sh'
     kubectl exec -it flagship -n warnet-armada -- bash -c 'echo "cd /golang" >> /run.sh'
     kubectl exec -it flagship -n warnet-armada -- bash -c 'echo "go install" >> /run.sh'
