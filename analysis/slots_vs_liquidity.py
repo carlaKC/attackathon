caf097be4e920cab9b3cb1656bf1c5d2349bcc33
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2024-08-05 15:28:40 -0400

    analysis: add unconditional fee check to graph processing script

--- analysis/slots_vs_liquidity.py
+++ analysis/slots_vs_liquidity.py
@@ -22,6 +22,42 @@ def should_slot_jam(local_amt, base_fee_msat, fee_rate_ppm, max_htlc, min_htlc):
 
     return liquidity_prepay + liquidity_uncond > slot_prepay + slot_uncond
 
+def change_in_uncond_fee(local_amt, base_fee_msat, fee_rate_ppm, max_htlc, min_htlc, base_premium_rate):
+    fast_jam_rounds = 10
+    height_delta = 160
+    resolution_period = 90
+    uncond_rate = 0.01
+
+    liquidity_htlc_fee = max_htlc * fee_rate_ppm / 1e6 + base_fee_msat
+    liquidity_prepay = liquidity_htlc_fee * height_delta * 60 * 10 / resolution_period * local_amt / max_htlc
+    liquidity_uncond = liquidity_htlc_fee * uncond_rate * local_amt / max_htlc * fast_jam_rounds
+
+    slot_htlc_fee = min_htlc * fee_rate_ppm / 1e6 + base_fee_msat
+    slot_prepay = slot_htlc_fee * height_delta * 60 * 10 / resolution_period * 242
+    slot_uncond = slot_htlc_fee * uncond_rate * 242 * fast_jam_rounds
+
+    base_premium = base_fee_msat * base_premium_rate
+    minimum_uncond_old = slot_htlc_fee * 0.01
+    minimum_uncond_new = minimum_uncond_old + base_premium
+
+    maximum_uncond_old = liquidity_htlc_fee * 0.01
+    maximum_uncond_new = maximum_uncond_old + base_premium
+
+    average_size_msat = 44_700_000
+    average_fee_msat = average_size_msat * fee_rate_ppm/1e6 + base_fee_msat
+    average_uncond_old = average_fee_msat * 0.01
+    average_uncond_new = average_uncond_old + base_premium
+    
+    if minimum_uncond_old ==0 or base_fee_msat == 0 and base_fee_msat == 2147483647:
+        return 0,0,0,False
+   
+    return (
+        minimum_uncond_new / slot_htlc_fee,
+        maximum_uncond_new / liquidity_htlc_fee,
+        average_uncond_new / average_fee_msat,
+        True,
+    )
+
 def parse_json_file(filename):
     with open(filename, 'r') as file:
         data = json.load(file)
@@ -30,6 +66,13 @@ def parse_json_file(filename):
 def main(filename):
     edges = parse_json_file(filename)
     counts = defaultdict(int)
+    fee_base_msat_values = []
+
+    min_increases = []
+    max_increases = []
+    avg_increases = []
+    
+    base_premium_rate = 0.1
 
     for edge in edges:
         # Convert sat to msat
@@ -46,6 +89,14 @@ def main(filename):
 
             source_slot_jamming = should_slot_jam(local_amt, source_base_fee_msat, source_fee_rate_ppm, source_max_htlc, source_min_htlc)
             counts[source_slot_jamming] += 1
+            
+            fee_base_msat_values.append(source_base_fee_msat)
+
+            min_inc, max_inc, avg_inc, ok = change_in_uncond_fee(local_amt, source_base_fee_msat, source_fee_rate_ppm, source_max_htlc, source_min_htlc, base_premium_rate)
+            if ok:
+                min_increases.append(min_inc)
+                max_increases.append(max_inc)
+                avg_increases.append(avg_inc)
 
         if node2_policy:
             target_base_fee_msat = int(node2_policy.get('fee_base_msat', 0))
@@ -55,9 +106,36 @@ def main(filename):
 
             target_slot_jamming = should_slot_jam(local_amt, target_base_fee_msat, target_fee_rate_ppm, target_max_htlc, target_min_htlc)
             counts[target_slot_jamming] += 1
-    
+
+            fee_base_msat_values.append(target_base_fee_msat)
+
+            min_inc, max_inc, avg_inc, ok = change_in_uncond_fee(local_amt, target_base_fee_msat, target_fee_rate_ppm, target_max_htlc, target_min_htlc, base_premium_rate)
+            if ok:
+                min_increases.append(min_inc)
+                max_increases.append(max_inc)
+                avg_increases.append(avg_inc)
+
     print(f"Should slot jam: {counts[True]}")
     print(f"Should liquidity jam: {counts[False]}")
+
+    
+    fee_base_msat_values.sort()
+    n = len(fee_base_msat_values)
+    if n > 0:
+        median_fee_base_msat = fee_base_msat_values[n // 2] if n % 2 == 1 else (fee_base_msat_values[n // 2 - 1] + fee_base_msat_values[n // 2]) / 2
+    else:
+        median_fee_base_msat = 0
+
+    print(f"Median base fee: {median_fee_base_msat}")
+
+   # Calculate averages
+    avg_min_increase = sum(min_increases) / len(min_increases)
+    avg_max_increase = sum(max_increases) / len(max_increases)
+    avg_avg_increase = sum(avg_increases) / len(avg_increases)
+
+    print(f"Average % of success for minimum htlc size: {avg_min_increase*100:.5}%")
+    print(f"Average % of success for maximum htlc size: {avg_max_increase*100:.5}%")
+    print(f"Average % of success fee for average payment: {avg_avg_increase*100:.5}%")
 
 if __name__ == "__main__":
     main(sys.argv[1])
